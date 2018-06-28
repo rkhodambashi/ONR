850a6ce98e603774a5895daf897fc3bdb51d1856
rkhodambashi@gmail.com <rkhodambashi@gmail.com>
2018-06-28 09:57:55 -0700

    added PCA9685_mosfetArray_manual.ino

--- Python 4leggedArray Control/4leggedArray.py
+++ Python 4leggedArray Control/4leggedArray.py
@@ -10,12 +10,92 @@ import time
 ser = serial.Serial('COM9', baudrate=115200, bytesize =8)
 print(ser.name) 
 time.sleep(5)
+
+firstLegRight = 1
+firstLegLeft = 2
+secondLegRight = 3
+secondLegLeft = 4
+thirdLegRight = 5
+thirdLegLeft = 6
+fourthLegRight = 7
+fourthLegLeft = 8
+timesleep = 0.3;
 while True:
-    for i in range (0,4095,100):
-        texts = '3,'+str(i)+','#this format is to match the corresponding arduino code 
-        ser.write(texts.encode('ASCII'))
-        time.sleep(0.01)
-    for i in range (4095,0,-100):
-        texts = '3,'+str(i)+','#this format is to match the corresponding arduino code 
-        ser.write(texts.encode('ASCII'))
-        time.sleep(0.01)
+    texts = str(firstLegRight)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(0.01)
+    texts = str(thirdLegRight)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    
+    time.sleep(timesleep)
+    
+    texts = str(firstLegLeft)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(0.01)
+    texts = str(thirdLegLeft)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    
+    time.sleep(timesleep)
+        
+    texts = str(firstLegRight)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(0.01)
+    texts = str(thirdLegRight)+',0,'
+    ser.write(texts.encode('ASCII'))
+    
+    time.sleep(timesleep)
+    
+    texts = str(firstLegLeft)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(0.01)
+    texts = str(thirdLegLeft)+',0,'
+    ser.write(texts.encode('ASCII'))
+    
+    time.sleep(timesleep)
+    
+    #--------------------------------------------------
+    texts = str(secondLegRight)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(0.01)
+    texts = str(fourthLegRight)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    
+    time.sleep(timesleep)
+    
+    texts = str(secondLegLeft)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(0.01)
+    texts = str(fourthLegLeft)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    
+    time.sleep(timesleep)
+    
+    texts = str(secondLegRight)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(0.01)
+    texts = str(fourthLegRight)+',0,'
+    ser.write(texts.encode('ASCII'))
+    
+    time.sleep(timesleep)
+    
+    texts = str(secondLegLeft)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(0.01)
+    texts = str(fourthLegLeft)+',0,'
+    ser.write(texts.encode('ASCII'))
+    
+    time.sleep(5)
+    
+#    texts = str(firstLegRight)+',4095,'
+#    ser.write(texts.encode('ASCII'))
+#    texts = str(firstLegRight)+',4095,'
+#    ser.write(texts.encode('ASCII'))
+    
+#    for i in range (0,4095,100):
+#        texts = '3,'+str(i)+','#this format is to match the corresponding arduino code 
+#        ser.write(texts.encode('ASCII'))
+#        time.sleep(0.01)
+#    for i in range (4095,0,-100):
+#        texts = '3,'+str(i)+','#this format is to match the corresponding arduino code 
+#        ser.write(texts.encode('ASCII'))
+#        time.sleep(0.01)
