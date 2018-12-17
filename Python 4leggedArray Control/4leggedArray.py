eb74073ecbbec77a77839e8761ae4ea66c1905eb
rkhodambashi@gmail.com <rkhodambashi@gmail.com>
2018-12-17 14:03:37 -0700

    image processing added

--- Python 4leggedArray Control/4leggedArray.py
+++ Python 4leggedArray Control/4leggedArray.py
@@ -11,80 +11,149 @@ ser = serial.Serial('COM3', baudrate=115200, bytesize =8)
 print(ser.name) 
 time.sleep(5)
 
-firstLegRight = 1
-firstLegLeft = 2
-secondLegRight = 3
-secondLegLeft = 4
-thirdLegRight = 5
-thirdLegLeft = 6
-fourthLegRight = 7
-fourthLegLeft = 8
+#firstLegRight = 1
+#firstLegLeft = 2
+#secondLegRight = 3
+#secondLegLeft = 4
+#thirdLegRight = 5
+#thirdLegLeft = 6
+#fourthLegRight = 7
+#fourthLegLeft = 8
+Resistor1 = 1
+Resistor2 = 2
+Resistor3 = 3
+Resistor4 = 4
+Resistor5 = 5
+Resistor6 = 6
+Resistor7 = 7
+Resistor8 = 8
+Resistor9 = 9
+Resistor10 = 10
+Resistor11 = 11
+Resistor12 = 12
+Resistor13 = 13
+Resistor14 = 14
+Resistor15 = 15
+Resistor16 = 16
+timeSleep = 0.1
+timeSleep1 = 1
+timeSleep2 = 0.01
+
 timesleep = 0.3;
 while True:
-    texts = str(firstLegRight)+',4095,'
+    texts = str(Resistor1)+',4095,'
     ser.write(texts.encode('ASCII'))
-    time.sleep(0.01)
-    texts = str(thirdLegRight)+',4095,'
+    time.sleep(timeSleep)
+    texts = str(Resistor1)+',0,'
     ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
     
-    time.sleep(timesleep)
-    
-    texts = str(firstLegLeft)+',4095,'
+    texts = str(Resistor2)+',4095,'
     ser.write(texts.encode('ASCII'))
-    time.sleep(0.01)
-    texts = str(thirdLegLeft)+',4095,'
+    time.sleep(timeSleep)
+    texts = str(Resistor2)+',0,'
     ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
     
-    time.sleep(timesleep)
+    texts = str(Resistor3)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor3)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor4)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor4)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor5)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor5)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor6)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor6)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor7)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor7)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor8)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor8)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor9)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor9)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor10)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor10)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor11)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor11)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor12)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor12)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor13)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor13)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor14)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor14)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor15)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor15)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    
+    texts = str(Resistor16)+',4095,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
+    texts = str(Resistor16)+',0,'
+    ser.write(texts.encode('ASCII'))
+    time.sleep(timeSleep)
         
-    texts = str(firstLegRight)+',0,'
-    ser.write(texts.encode('ASCII'))
-    time.sleep(0.01)
-    texts = str(thirdLegRight)+',0,'
-    ser.write(texts.encode('ASCII'))
-    
-    time.sleep(timesleep)
-    
-    texts = str(firstLegLeft)+',0,'
-    ser.write(texts.encode('ASCII'))
-    time.sleep(0.01)
-    texts = str(thirdLegLeft)+',0,'
-    ser.write(texts.encode('ASCII'))
-    
-    time.sleep(timesleep)
-    
-    #--------------------------------------------------
-    texts = str(secondLegRight)+',4095,'
-    ser.write(texts.encode('ASCII'))
-    time.sleep(0.01)
-    texts = str(fourthLegRight)+',4095,'
-    ser.write(texts.encode('ASCII'))
-    
-    time.sleep(timesleep)
-    
-    texts = str(secondLegLeft)+',4095,'
-    ser.write(texts.encode('ASCII'))
-    time.sleep(0.01)
-    texts = str(fourthLegLeft)+',4095,'
-    ser.write(texts.encode('ASCII'))
-    
-    time.sleep(timesleep)
-    
-    texts = str(secondLegRight)+',0,'
-    ser.write(texts.encode('ASCII'))
-    time.sleep(0.01)
-    texts = str(fourthLegRight)+',0,'
-    ser.write(texts.encode('ASCII'))
-    
-    time.sleep(timesleep)
-    
-    texts = str(secondLegLeft)+',0,'
-    ser.write(texts.encode('ASCII'))
-    time.sleep(0.01)
-    texts = str(fourthLegLeft)+',0,'
-    ser.write(texts.encode('ASCII'))
-    
-    time.sleep(5)
+    time.sleep(timeSleep1)
     
 #    texts = str(firstLegRight)+',4095,'
 #    ser.write(texts.encode('ASCII'))
